fd9497bdaad26ff5e120095599bb268a0f7e3948
lovelivesimply <1047723382@qq.com>
2017-09-11 09:51:40 +0800

    finish the getTheBookList

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,15 +1,27 @@
 package com.twu.biblioteca;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
 
 
     public static void main(String[] args) {
-        GetWelcomeMessage();
+        getWelcomeMessage();
     }
 
-    public static void GetWelcomeMessage(){
+    public static void getWelcomeMessage(){
         System.out.println("Welcome to the Biblioteca library!");
+    }
+    public static void getBookLists(){
+        List<String> bookList=new ArrayList<String>();
+        bookList.add("thinking in java");
+        bookList.add("head first java");
+        for(int i=0;i<bookList.size();i++){
+            System.out.print(i+1+":");
+            System.out.println(bookList.get(i));
+        }
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -5,7 +5,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 
-import static com.twu.biblioteca.BibliotecaApp.GetWelcomeMessage;
+import static com.twu.biblioteca.BibliotecaApp.getBookLists;
+import static com.twu.biblioteca.BibliotecaApp.getWelcomeMessage;
 import static org.junit.Assert.assertEquals;
 public class ExampleTest {
 
@@ -14,9 +15,15 @@ public class ExampleTest {
     public final SystemOutRule log = new SystemOutRule().enableLog();
 
     @Test
-    public void test() {
+    public void should_return_welcomeMessage_when_login() {
         log.clearLog();
-        GetWelcomeMessage();
+        getWelcomeMessage();
         assertEquals(log.getLog(), ("Welcome to the Biblioteca library!\n"));
+    }
+    @Test
+    public void ß() {
+        log.clearLog();
+        getBookLists();
+        assertEquals(log.getLog(), ("1:thinking in java\n2:head first java\n"));
     }
 }
